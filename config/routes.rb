a80b33b73a94cee97e2a357360bfc3585eb77022
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 02:05:40 +1100

    routes finalised

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'lessons#index'
   resources :lessons, only:[:index, :show, :new, :create, :destroy, :edit, :update] do
-  resources :sessions, only:[:index, :show, :destroy, :update, :new, :create]
+  resources :sessions, only:[:index,:show, :destroy, :update, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
