95da12355c58d3f7dc967bb88382b558c23ef8fb
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 02:05:11 +1100

    add sessions association, has_many: sessions, through::lessons

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-        has_many :lessons
+  has_many :lessons
+  has_many :seesions, through: :lessons
 end
