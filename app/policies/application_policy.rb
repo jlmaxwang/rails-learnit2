deb01205fc172bb1a3da1c3590439cce255bfb12
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 23:24:19 +1100

    inheritied poly g by pundit

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,53 +1,112 @@
 # frozen_string_literal: true
 
 # class ApplicationPolicy
-#   attr_reader :user, :record
+  # attr_reader :user, :record
 
-#   def initialize(user, record)
-#     @user = user
-#     @record = record
-#   end
+  # def initialize(user, record)
+  #   @user = user
+  #   @record = record
+  # end
 
-#   def index?
-#     false
-#   end
+  # def index?
+  #   false
+  # end
 
-#   def show?
-#     false
-#   end
+  # def show?
+  #   scope.where(id: record.id).exists?
+  # end
 
-#   def create?
-#     false
-#   end
+  # def create?
+  #   false
+  # end
 
-#   def new?
-#     create?
-#   end
+  # def new?
+  #   create?
+  # end
 
-#   def update?
-#     false
-#   end
+  # def update?
+  #   false
+  # end
 
-#   def edit?
-#     update?
-#   end
+  # def edit?
+  #   update?
+  # end
 
-#   def destroy?
-#     false
-#   end
+  # def destroy?
+  #   false
+  # end
 
-#   class Scope
-#     def initialize(user, scope)
-#       @user = user
-#       @scope = scope
-#     end
+  # def scope
+  #   Pundit.policy_scope!(user, record.class)
+  # end
 
-#     def resolve
-#       raise NotImplementedError, "You must define #resolve in #{self.class}"
-#     end
+  # class Scope
+  #   def initialize(user, scope)
+  #     @user = user
+  #     @scope = scope
+  #   end
 
-#     private
+  #   def resolve
+  #     raise NotImplementedError, "You must define #resolve in #{self.class}"
+  #   end
 
-#     attr_reader :user, :scope
-#   end
+  #   private
+
+  #   attr_reader :user, :scope
+  # end
+
 # end
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    # scope.where(:id => record.id).exists?
+    scope.where(id: record.id).exists?
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
