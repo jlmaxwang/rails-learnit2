a81a3c3d7b410618cccd3e40a23821ac3af32769
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 23:26:43 +1100

    session policy set to current_user action of show,create,update,destroy

---
+++ app/policies/session_policy.rb
@@ -0,0 +1,30 @@
+class SessionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    record.user == user  # Anyone can view a session
+  end
+  def new?
+    true
+  end
+
+  def edit?
+    true
+  end
+
+  def create?
+    record.user == user  # only lesson creator can create a session
+  end
+
+  def update?
+    record.user == user  # Only session creator can update it
+  end
+
+  def destroy?
+    record.user == user  # Only session creator can update it
+  end
+end
