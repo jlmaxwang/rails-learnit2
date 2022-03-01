2d2cb194cb13a88343642080dca04532d107df4a
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 17:12:31 +1100

    session route changed, deleted session#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :lessons, only:[:index, :show, :new, :create, :destroy, :edit, :update] do
-  resources :sessions, only:[:index,:show, :destroy, :update, :new, :create]
+  resources :sessions, only:[:index, :destroy, :update, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
