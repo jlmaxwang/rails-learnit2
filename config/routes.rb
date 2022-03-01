352ba642270d88b7e9fa2fe67b2ac6ec768c54aa
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-01 23:36:09 +1100

    merge home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'lessons#index'
   resources :lessons, only:[:index, :show, :new, :create, :destroy, :edit, :update] do
   resources :sessions, only:[:index, :destroy, :update, :new, :create]
   end
