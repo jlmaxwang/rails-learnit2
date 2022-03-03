5ba659e307870eb6662f57464695e5b8c33c3587
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-03 14:01:11 +1100

    home page now routes back to pages#home, also added routes for user dash

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'lessons#index'
+  root to: 'pages#home'
   resources :lessons, only:[:index, :show, :new, :create, :destroy, :edit, :update] do
   resources :sessions, only:[:index, :destroy, :update, :new, :create]
   end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
